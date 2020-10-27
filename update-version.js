a7fdb89a3a3c77af67d8c08564f61020f618bf06
getlarge <ed@getlarge.eu>
2020-10-27 15:08:10 +0100

    fix schema $id versioning

--- update-version.js
+++ update-version.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
 const { promisify } = require('util');
 
-const serverUrl = 'https://raw.githubusercontent.com/s1seven/EN10168-schemas';
-// TODO: when schema-deploys stable
-// const serverUrl = 'https://schemas.en10204.io/EN10168';
+const serverUrl = 'https://schemas.en10204.io/EN10168-schemas';
+const schemaFilePaths = ['schema.json'];
 
 function readFile(path) {
   return promisify(fs.readFile)(path, 'utf8');
@@ -13,14 +12,14 @@ function writeFile(path, data) {
   return promisify(fs.writeFile)(path, data);
 }
 
-const schemaFilePaths = ['schema.json'];
-
 async function updateSchemasVersion(version) {
   await Promise.all(
     schemaFilePaths.map(async (filePath) => {
       const schema = JSON.parse(await readFile(filePath));
-      const [schemaName] = filePath.split('.');
-      schema.$id = `${serverUrl}/${version}/${schemaName}.schema.json`;
+      let [schemaName] = filePath.split('.');
+      schemaName =
+        schemaName === 'schema' ? schemaName : `${schemaName}.schema`;
+      schema.$id = `${serverUrl}/${version}/${schemaName}.json`;
       await writeFile(filePath, JSON.stringify(schema, null, 2));
     })
   );
