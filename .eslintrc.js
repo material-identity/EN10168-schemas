3c43350545b88c5272ecdc21bc026fa38fb2f066
getlarge <ed@getlarge.eu>
2020-11-16 08:23:29 +0100

    feat: add generate-pdf minified build

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
   },
+  ignorePatterns: ['**/*.min.js'],
   extends: [
     'eslint:recommended',
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
