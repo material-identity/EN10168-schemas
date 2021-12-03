ad6aa07734986e95135a95b3ea1ee8325c7710c4
getlarge <ed@getlarge.eu>
2021-12-03 14:04:33 +0100

    fix: remove console.log

--- utils/generate-pdf-cli.js
+++ utils/generate-pdf-cli.js
@@ -3,11 +3,10 @@ const { generatePdfCertificate } = require('./generate-pdf');
 
 (async function (argv) {
   const certificatePattern = argv[2] || 'test/fixtures/valid_certificate_*.json';
-  console.log(__dirname);
   try {
     const filePaths = glob.sync(certificatePattern);
     await Promise.all(filePaths.map((filePath) => generatePdfCertificate(filePath)));
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
   }
 })(process.argv);
